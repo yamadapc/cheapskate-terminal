e8524ddf8d4a10b277789890e2a6991138824aac
yamadapc <tacla.yamada@gmail.com>
2015-12-17 19:07:45 -0200

    Add buffering to `stdout`
    This makes things a lot faster.

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,10 @@ import           Cheapskate
 import           Cheapskate.Terminal
 import qualified Data.Text.IO        as Text (readFile)
 import           System.Environment  (getArgs)
+import           System.IO
 
 main :: IO ()
 main = do
     (f:_) <- getArgs
+    hSetBuffering stdout (BlockBuffering (Just 500))
     prettyPrint =<< markdown def <$> Text.readFile f
