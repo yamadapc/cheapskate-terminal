9d4d63a4a708540caa3ca5e713aaa334c06708a7
yamadapc <tacla.yamada@gmail.com>
2016-08-28 11:04:34 -0300

    Support reading stdin

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,15 @@ module Main where
 
 import           Cheapskate
 import           Cheapskate.Terminal
-import qualified Data.Text.IO        as Text (readFile)
+import qualified Data.Text.IO        as Text (readFile, getContents)
 import           System.Environment  (getArgs)
 import           System.IO
 
 main :: IO ()
 main = do
-    (f:_) <- getArgs
     hSetBuffering stdout (BlockBuffering (Just 500))
-    prettyPrint =<< markdown def <$> Text.readFile f
+    args <- getArgs
+    input <- case args of
+        (f:_) -> Text.readFile f
+        _-> Text.getContents
+    prettyPrint (markdown def input)
